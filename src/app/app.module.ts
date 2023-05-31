5f6c165a021cc7f67a46515958beff2cf1edc988
VictorCstr <victorcastro.dev@hotmail.com>
2023-05-31 10:16:18 -0300

    feat: first componentes: header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { ListComponent } from './components/list/list.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginFormComponent,
-    ListComponent
+    ListComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
