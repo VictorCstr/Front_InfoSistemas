aceed412acd4eb3c56af153484e9157a5ea8b051
VictorCstr <victorcastro.dev@hotmail.com>
2023-05-31 09:27:45 -0300

    feat: routes setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { ListComponent } from './components/list/list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginFormComponent },
+  { path: 'painel', component: ListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFormComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
